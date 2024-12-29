2ca496f99690388753a6f98186edfc746016326a
Masha159 <54938822+Masha159@users.noreply.github.com>
2024-12-29 22:55:05 +0100

    add converter for Fahrenheit to Celsius.

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -1,7 +1,17 @@
 package lesson3;
 
 public class Main {
-    public static void main(String[] args) {
+    private static final double CONV_F = 0.5556;
 
+    public static void main(String[] args) {
+        System.out.println("Converter App.");
+        System.out.println("Version 1.2.");
+        double fhrnht = 68;
+        double clssFromFhrnht = convFhrnhtToClss(fhrnht);
+        System.out.println("Result is " + clssFromFhrnht + " Fahrenheit.");
+    }
+
+    private static double convFhrnhtToClss(double fhrnht) {
+        return CONV_F * (fhrnht - 32);
     }
 }
