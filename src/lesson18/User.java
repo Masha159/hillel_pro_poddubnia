460fb16bae670d0f29b3295853fba24d1e1b7585
Masha159 <54938822+Masha159@users.noreply.github.com>
2025-03-18 14:05:58 +0100

    lesson 18

---
+++ src/lesson18/User.java
@@ -0,0 +1,29 @@
+package lesson18;
+
+public class User {
+    int id;
+    String name;
+    String email;
+
+    public int getId() {
+        return id;
+    }
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
